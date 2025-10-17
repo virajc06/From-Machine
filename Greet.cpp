d1c08c392073ffb053dbf03c26d0b25d11830baa
virajc06 <virajchintakindi@gmail.com>
2025-10-17 17:06:25 +0530

    Updated to Voting Eligibility

--- Greet.cpp
+++ Greet.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-    char name[10];
-    cout<<"Enter Your Name : \n";
-    cin>>name;
+    char name[30];
+    int age;
+    cout<<"Enter Your Name and Age : \n";
+    cin>>name>>age;
     cout<<"Hello "<<name<<"\n";
+    if (age>=18)
+        cout<<"You can VOTE.";
+    else
+        cout<<"You cant Vote.";
     return 0;
 }
